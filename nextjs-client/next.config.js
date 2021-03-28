c9a1f81976cb5c2100dba1c4bfd9e9f6e6c41fe0
flolu <loflude@gmail.com>
2021-03-28 16:56:17 +0200

    feat(nextjs): basic pwa support

--- nextjs-client/next.config.js
+++ nextjs-client/next.config.js
@@ -1,8 +1,18 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
   /**
    * Compression is handled by the Kubernetes NGINX Ingress
    * Thus, the Next.js server has more resources to do its thing
    */
   compress: false,
   poweredByHeader: false,
-}
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    runtimeCaching,
+  },
+})
