67b28c4c3b06f213dd5f928029c455a8d3762c51
flolu <loflude@gmail.com>
2021-03-29 12:11:31 +0200

    feat(nextjs): fetch messages from server

--- nextjs-client/pages/discussions.tsx
+++ nextjs-client/pages/discussions.tsx
@@ -1,6 +1,20 @@
+import {GetServerSideProps, InferGetServerSidePropsType} from 'next'
 import Head from 'next/head'
 
-export default function Discussions() {
+export const getServerSideProps: GetServerSideProps = async () => {
+  // TODO try to access database from here directly?!
+  const res = await fetch('http://localhost:3001/messages')
+  const data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+  }
+
+}
+
+export default function Discussions({data}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       <Head>
@@ -9,6 +23,7 @@ export default function Discussions() {
 
       <main>
         <h1>Discussions</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </main>
     </div>
   )
